0192d674cfb9bec8616b14e360ed5bd673a817f0
Danilo Alves <danilo.alves@kinvo.com.br>
2020-09-05 19:45:10 -0300

    task(infra): Adds types for success and error requests

---
+++ src/infra/types.ts
@@ -0,0 +1,14 @@
+export interface RequestSuccessfullInterface<DataType> {
+    ok: boolean;
+    data?: DataType;
+    error: null;
+}
+
+export interface RequestErrorInterface {
+    ok: boolean;
+    data: null;
+    error: {
+        code: number;
+        message: string;
+    };
+}
