051f5d9276ed34eb1d2abf87c27832a542c055be
Danilo Alves <danilo.alves@kinvo.com.br>
2020-09-05 19:45:29 -0300

    feat(database): Creates connection with database

---
+++ src/database/index.ts
@@ -0,0 +1,13 @@
+// Dependencies
+import { Pool } from 'pg';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+export const connection = new Pool({
+    connectionString: process?.env?.DATABASE_URL,
+});
+
+connection.on('connect', () => {
+    console.log('Database connected successfully');
+});
